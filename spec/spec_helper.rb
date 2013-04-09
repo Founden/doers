32b2ee14267018fefcaea645a01e341e35cb1e97
Stas SUȘCOV <stas@net.utcluj.ro>
2013-04-09 13:30:23 +0300

    Enable simplecov gem.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+# Use simplecov only outside guard
+unless ENV['GUARD_NOTIFICATIONS']
+  require 'simplecov'
+  SimpleCov.start 'rails'
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
