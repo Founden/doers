3fcbda5fbf5dcd5f8a51df1034325dfc36e6a8d0
Stas SUȘCOV <stas@net.utcluj.ro>
2013-04-11 02:11:23 +0300

    Fixed session store initializer after rails upgrade.

--- config/initializers/0_session_store.rb
+++ config/initializers/0_session_store.rb
@@ -1,4 +1,2 @@
 # Be sure to restart your server when you modify this file.
-
-Doers::Application.config.session_store(
-  :encrypted_cookie_store, key: '_doers_session')
+Doers::Application.config.session_store(:cookie_store, key: '_doers_session')
