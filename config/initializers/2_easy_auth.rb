8d23d73b65b2aa4574c696bf5ebd29781863eaa1
Stas SUȘCOV <stas@net.utcluj.ro>
2013-04-14 00:45:34 +0300

    Update easy_auth twitter module.

--- config/initializers/2_easy_auth.rb
+++ config/initializers/2_easy_auth.rb
@@ -9,7 +9,7 @@ end
 # Overwrite some twitter oauth identity class methods
 Identities::Oauth::Twitter.instance_eval do
   def account_attributes_map
-    { :uid => 'screen_name', :screen_name => 'screen_name' }
+    { :email => 'screen_name' }
   end
 
   def retrieve_uid(token)
