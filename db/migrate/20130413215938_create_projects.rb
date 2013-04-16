cc233c69f151f0179005516d97e00837207e4961
Stas SUȘCOV <stas@net.utcluj.ro>
2013-04-16 19:25:38 +0300

    Project desription should be of type text.

--- db/migrate/20130413215938_create_projects.rb
+++ db/migrate/20130413215938_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
       t.string :title
-      t.string :description
+      t.text :description
       t.references :user, index: true
       t.string :status
 
