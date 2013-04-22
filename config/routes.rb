2c62f1a64269a70001f3d2748194fb07565e36bc
Stas SUȘCOV <stas@net.utcluj.ro>
2013-04-22 19:19:56 +0300

    Added doorkeeper to handle api endpoints.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Doers::Application.routes.draw do
+  # OAuth 2.0 endpoints
+  use_doorkeeper do
+    skip_controllers :applications, :authorized_applications
+  end
+
+  # Easy Auth endpoints
   easy_auth_routes
+
   get 'sign_in' => 'sessions#index'
 
   resources :profiles, :only => [:show, :update]
