f773b515411974d8ceb512931f90a1766c5e8557
Stas SUȘCOV <stas@net.utcluj.ro>
2013-04-29 14:51:05 +0300

    Added mine route to profiles controller and authentication check.

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -1,13 +1,36 @@
 require 'spec_helper'
 
 describe ProfilesController do
+  let(:user) { Fabricate(:user) }
 
   describe '#show' do
     before do
-      get(:show, :id => 1)
+      controller.stub(:current_account) { user }
+      get(:show, :id => (user.id rescue 1))
     end
 
     it { should render_template(:show) }
+
+    context 'as an authenticated visitor' do
+      let(:user) { nil }
+
+      it { should redirect_to(sign_in_path) }
+    end
+  end
+
+  describe '#mine' do
+    before do
+      controller.stub(:current_account) { user }
+      get(:mine)
+    end
+
+    it { should render_template(:mine) }
+
+    context 'as an authenticated visitor' do
+      let(:user) { nil }
+
+      it { should redirect_to(sign_in_path) }
+    end
   end
 
 end
