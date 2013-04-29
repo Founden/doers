ebf6e97d638c7e4e11080b36f85f467c485027ce
Stas SUȘCOV <stas@net.utcluj.ro>
2013-04-29 14:48:09 +0300

    Fixed fabricator typos.

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -2,5 +2,5 @@ Fabricator(:user) do
   name                    { sequence(:name) { |nid| Faker::Name.name } }
   email                   { sequence(:email) { |eid| Faker::Internet.email } }
   identities(:count => 1) { |attrs, i|
-    Fabricate(:password_identity, :uid => attrs[:email]) }
+    Fabricate(:twitter_identity, :uid => attrs[:email]) }
 end
