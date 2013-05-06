49d73ec89d06f0d347f0a4dfd8aaec476ccaf53c
Stas SUȘCOV <stas@net.utcluj.ro>
2013-05-06 21:34:19 +0300

    Added more project specs for update.

--- spec/controllers/api/v1/projects_controller_spec.rb
+++ spec/controllers/api/v1/projects_controller_spec.rb
@@ -34,4 +34,51 @@ describe Api::V1::ProjectsController do
       api_project['user']['id'].should eq(user.id)
     end
   end
+
+  describe '#create' do
+    let(:prj_attrs) { Fabricate.attributes_for(:project) }
+    before { post(:create, :project => prj_attrs) }
+
+    it 'creates a project and serializes it to json' do
+      project = JSON.parse(response.body)['project']
+      project.keys.count.should eq(6)
+
+      project['id'].should_not be_nil
+      project['title'].should eq(prj_attrs['title'])
+      project['description'].should eq(prj_attrs['description'])
+      project['status'].should eq(Project::STATES.first)
+      project['user']['id'].should eq(user.id)
+    end
+
+    context 'except on wrong arguments' do
+      let( :prj_attrs ) { {:project => {:title => ''} } }
+
+      it 'returns errors' do
+        project = JSON.parse(response.body)
+        project.keys.count.should eq(1)
+        project['errors']['title'].should_not be_blank
+      end
+    end
+  end
+
+  describe '#update' do
+    let(:prj) { Fabricate(:project, :user => user) }
+    let(:prj_attrs) do
+      Fabricate.attributes_for(:project, :status => Project::STATES.last)
+    end
+
+    before { post(:update, :id => prj.id, :project => prj_attrs) }
+
+    it 'creates a project and serializes it to json' do
+      project = JSON.parse(response.body)['project']
+      project.keys.count.should eq(6)
+
+      project['id'].should eq(prj.id)
+      project['title'].should eq(prj_attrs['title'])
+      project['description'].should eq(prj_attrs['description'])
+      project['status'].should eq(Project::STATES.last)
+      project['user']['id'].should eq(user.id)
+    end
+  end
+
 end
