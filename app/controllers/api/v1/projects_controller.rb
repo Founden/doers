7d39cb4d1b8284c3421c1d9c3c0a9cd825dfee11
Stas SUȘCOV <stas@net.utcluj.ro>
2013-05-06 21:34:19 +0300

    Added more project specs for update.

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -10,4 +10,28 @@ class Api::V1::ProjectsController < Api::V1::ApplicationController
   def show
     render :json => current_account.projects.where(params[:id]).first
   end
+
+  # Handles project creation
+  def create
+    project = current_account.projects.build(project_params)
+    if project.save
+      render :json => project
+    else
+      render :json => { :errors => project.errors.messages }
+    end
+  end
+
+  # Handles project changes
+  def update
+    project = current_account.projects.find(params[:id])
+    project.update_attributes(project_params)
+    render :json => project
+  end
+
+  private
+
+    # Strong parameters for project object
+    def project_params
+      params.require(:project).permit(:title, :description, :status)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Doers::Application.routes.draw do
   namespace :api, :constraints => {:format => :json} do
     namespace :v1 do
       resources(:users, :only => [:index, :show])
-      resources(:projects, :only => [:index, :show])
+      resources(:projects, :only => [:index, :show, :create, :update])
     end
   end
 
