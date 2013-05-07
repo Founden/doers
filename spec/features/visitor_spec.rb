02d3891894d77e74b7e4cd01f1934ea5b8344472
Stas SUȘCOV <stas@net.utcluj.ro>
2013-05-07 22:01:51 +0300

    Added visitor spec.

---
+++ spec/features/visitor_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+feature 'Visitor', :js do
+
+  scenario 'is asked to sign in before accessing a non-public page' do
+    visit dashboard_pages_path
+
+    current_path.should eq(sign_in_path)
+  end
+end
