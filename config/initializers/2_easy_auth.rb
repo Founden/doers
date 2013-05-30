2e16aae9951cd1120af68b9f68b05a293b1ab162
Stas SUȘCOV <stas@net.utcluj.ro>
2013-05-30 13:03:29 +0300

    Added data store to user to handle misc stuff.

--- config/initializers/2_easy_auth.rb
+++ config/initializers/2_easy_auth.rb
@@ -5,3 +5,10 @@ EasyAuth.config do |c|
     Doers::Config.oauth2_providers.angel_list.secret_key
   )
 end
+
+
+module EasyAuth::Models::Identities::Oauth2::AngelList
+  def account_attributes_map
+    { :email => 'email', :name => 'name', :angel_list_id => 'id' }
+  end
+end
