1160e69a9198f270f757eec0ff4eb4a687ebffb4
Stas SUȘCOV <stas@net.utcluj.ro>
2013-06-04 12:22:08 +0300

    Ignore a test host to be used by webmock.

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,5 +5,6 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.default_cassette_options = { :record => :new_episodes }
   c.ignore_localhost = true
+  c.ignore_hosts 'test.example.com'
   c.configure_rspec_metadata!
 end
