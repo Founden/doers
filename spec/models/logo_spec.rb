f6f0ef33743b63182d8126c79b444cdb8f30c2d4
Stas SUȘCOV <stas@net.utcluj.ro>
2013-06-04 12:21:54 +0300

    s/image/logo/ spec

--- spec/models/logo_spec.rb
+++ spec/models/logo_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require 'paperclip/matchers'
 
 describe Logo do
   include Paperclip::Shoulda::Matchers
@@ -27,6 +26,23 @@ describe Logo do
     subject { Fabricate.build(:logo, :description => bad_input) }
 
     its(:description){ should_not match(/\<\>/) }
+  end
+
+  context 'attachment must be downloaded' do
+    let(:logo_url) { URI.parse('http://test.example.com/test.png') }
+    let(:logo) { Fabricate.build(:logo, :attachment => logo_url) }
+
+    subject { logo }
+
+    before do
+      stub_request(:get, logo_url.to_s).to_return(
+        :body => File.read(Rails.root.join('spec/fixtures/test.png')),
+        :headers => { 'Content-Type' => 'image/png' }
+      )
+      logo.save
+    end
+
+    its(:attachment) { should_not be_nil }
   end
 
 end
