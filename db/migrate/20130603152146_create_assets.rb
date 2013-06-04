60f0b4122a1a69be265dc03b93e94262fe0e35c6
Stas SUȘCOV <stas@net.utcluj.ro>
2013-06-04 17:47:50 +0300

    Cleanup assets migration.

--- db/migrate/20130603152146_create_assets.rb
+++ db/migrate/20130603152146_create_assets.rb
@@ -8,11 +8,7 @@ class CreateAssets < ActiveRecord::Migration
       t.string :type, :index => true
       t.integer :assetable_id, :index => true
       t.string :assetable_type, :index => true
-      t.string :attachment_file_name
-      t.string :attachment_remote_url
-      t.string :attachment_content_type
-      t.integer :attachment_file_size
-      t.datetime :attachment_updated_at
+      t.attachment :attachment
     end
   end
 end
