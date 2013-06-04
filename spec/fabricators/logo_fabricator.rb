5d3e84b131a81bc9bef6fa1a46de6445504e724b
Stas SUȘCOV <stas@net.utcluj.ro>
2013-06-04 12:21:37 +0300

    Use a real file instead of a fixture.

--- spec/fabricators/logo_fabricator.rb
+++ spec/fabricators/logo_fabricator.rb
@@ -2,6 +2,5 @@ Fabricator(:logo) do
   project
   user
   description { sequence(:description) { Faker::Lorem.paragraph } }
-  attachment  { Rack::Test::UploadedFile.new(
-      Rails.root.join('spec/fixtures/test.png'), 'image/png') }
+  attachment  { File.open(Rails.root.join('spec/fixtures/test.png')).close }
 end
