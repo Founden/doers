c3048963df5bfb5004e9eb90daaaa11d8f4232e8
Stas SUȘCOV <stas@net.utcluj.ro>
2013-06-14 13:26:15 +0300

    Updated tests for projects creation.

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -6,6 +6,38 @@ feature 'Projects', :js, :slow, :vcr => {:cassette_name=>:angel_list_oauth2} do
     sign_in_with_angel_list
   end
 
+  scenario 'user can create a project' do
+    prj_attrs = Fabricate.attributes_for(:project)
+    visit root_path(:anchor => 'projects/new')
+
+    within('#project-new') do
+      fill_in :titleInput, :with => prj_attrs[:title]
+      fill_in :wwwInput, :with => prj_attrs[:website]
+      fill_in :descriptionInput, :with => prj_attrs[:description]
+    end
+
+    click_on('project-save')
+
+    expect(page).to have_css('#projects .project')
+    expect(page).to have_content(prj_attrs[:title])
+  end
+
+  scenario 'user can not create a project if attributes are missing' do
+    title = Faker::Lorem.word
+    visit root_path(:anchor => 'projects/new')
+
+    within('#project-new') do
+      fill_in :titleInput, :with => title
+    end
+
+    click_on('project-save')
+
+    expect(page).to have_css('#notifications .alert-box.alert')
+    visit root_path(:anchor => 'dashboard')
+    expect(page).to_not have_content(title)
+  end
+
+
   scenario 'user can access an existing project' do
     2.times { Fabricate(:project, :user => User.first) }
 
@@ -15,22 +47,6 @@ feature 'Projects', :js, :slow, :vcr => {:cassette_name=>:angel_list_oauth2} do
   end
 
 
-  scenario 'user creates a new project' do
-    prj_attrs = Fabricate.attributes_for(:project)
-
-    visit root_path(:anchor => :dashboard)
-
-    expect(page).to have_css('#projects li', :count => 0)
-
-    click_on('projects-add')
-    fill_in('titleInput', :with => prj_attrs[:title])
-    fill_in('descriptionInput', :with => prj_attrs[:description])
-    click_on('projects-save')
-    visit root_path(:anchor => :dashboard)
-
-    expect(page).to have_css('#projects .project', :count => 1)
-  end
-
   scenario 'user can access an existing project' do
     projects = 2.times.collect { Fabricate(:project, :user => User.first) }
 
@@ -38,5 +54,9 @@ feature 'Projects', :js, :slow, :vcr => {:cassette_name=>:angel_list_oauth2} do
     find('#project-%d a' % projects.first.id).click
 
     expect(page).to have_content(projects.first.title.upcase)
+  end
+
+  scenario 'importer for Angel List projects' do
+    pending 'jQuery.ajax needs mocking, which is pretty hard to do from Ruby'
   end
 end
