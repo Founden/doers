8b1cf339414ae5d93e79d518dc943a7bdba07dba
Stas SUȘCOV <stas@net.utcluj.ro>
2013-06-14 17:38:09 +0300

    Use :new_episodes on vcr to avoid exceptions.

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,9 +3,9 @@ require 'vcr'
 VCR.configure do |c|
   c.hook_into :webmock
   c.cassette_library_dir = 'spec/cassettes'
-  c.default_cassette_options = { :record => :none }
+  c.default_cassette_options = { :record => :new_episodes }
   c.ignore_localhost = true
-  c.ignore_hosts ['test.example.com', 'fonts.googleapis.com',
-                  'www.gravatar.com', 's3.amazonaws.com']
+  c.ignore_hosts 'test.example.com', 'fonts.googleapis.com',
+    'www.gravatar.com', 's3.amazonaws.com'
   c.configure_rspec_metadata!
 end
