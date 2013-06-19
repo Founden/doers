1e637a95d92237aab0057b43a332b5587915d9c2
Stas SUȘCOV <stas@net.utcluj.ro>
2013-06-19 20:30:17 +0300

    Added a fabricator for the administrative user.

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -6,3 +6,7 @@ Fabricator(:user) do
   identities(:count => 1) { |attrs, i|
     Fabricate(:angel_list_identity, :uid => attrs[:email]) }
 end
+
+Fabricator(:admin, :from => :user) do
+  email                   { 'test@geekcelerator.com' }
+end
