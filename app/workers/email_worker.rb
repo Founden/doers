ab68b34f85a2435feb055706910c1ec739b3afd6
Stas SUȘCOV <stas@net.utcluj.ro>
2013-06-19 20:47:48 +0300

    Search for root namespace to avoid wrong resolving.

--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -5,8 +5,8 @@ class EmailWorker
   # Worker tasks go here
   def perform(method, user_id)
     ActiveRecord::Base.connection_pool.with_connection do
-      user = User.find(user_id)
-      UserMailer.send(method.to_sym, user).deliver
+      user = ::User.find(user_id)
+      ::UserMailer.send(method.to_sym, user).deliver
     end
   end
 end
