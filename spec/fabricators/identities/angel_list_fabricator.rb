365361303197a0c5362ad672ca913ee855f157f5
Stas SUȘCOV <stas@net.utcluj.ro>
2013-06-24 12:52:59 +0300

    Identity fabricator should provide a token we can save in db.

--- spec/fabricators/identities/angel_list_fabricator.rb
+++ spec/fabricators/identities/angel_list_fabricator.rb
@@ -1,3 +1,4 @@
 Fabricator(:angel_list_identity, :class_name=>Identities::Oauth2::AngelList) do
   uid       { sequence(:uid_email) { |eid| Faker::Internet.email } }
+  token     { 'DUMMY_TOKEN' }
 end
