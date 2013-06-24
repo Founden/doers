5f89453730ae23a5a18274d27af224687e753e31
Stas SUȘCOV <stas@net.utcluj.ro>
2013-06-24 13:12:23 +0300

    Added comments serizalized attribute to handle metadata from imports.

--- spec/fabricators/comment_fabricator.rb
+++ spec/fabricators/comment_fabricator.rb
@@ -8,3 +8,11 @@ end
 Fabricator(:comment_with_parent, :from => :comment) do
   parent_comment(:fabricator => :comment)
 end
+
+Fabricator(:comment_from_angel_list, :class_name => Comment) do
+  project
+  content                 { Faker::HTMLIpsum.fancy_string }
+  angel_list_id           { 1234 }
+  angel_list_author_name  { Faker::Name.name }
+  angel_list_author_id    { 123 }
+end
