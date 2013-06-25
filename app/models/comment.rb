96c753709c5391d39b9770501b5564ede1f60fa7
Stas SUȘCOV <stas@net.utcluj.ro>
2013-06-25 16:35:44 +0300

    Added comments serizalized attribute to handle metadata from imports.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,10 @@
 # DOERS [Comment] class
 class Comment < ActiveRecord::Base
+  # TODO: Change this to an hstore when in production
+  store :data, :coder => JSON
+  store_accessor :data, :angel_list_id
+  store_accessor :data, :angel_list_author_name, :angel_list_author_id
+
   # Relationships
   belongs_to :project
   belongs_to :board
@@ -14,5 +19,19 @@ class Comment < ActiveRecord::Base
   # Sanitize user input
   before_validation do
     self.content = Sanitize.clean(self.content)
+  end
+
+  # Handles user name in case the comment was imported
+  # @return OpenStruct to mock user fields
+  def author
+    user || OpenStruct.new({
+      :nicename => angel_list_author_name,
+      :email => "%s@%s" % [angel_list_author_id, Doers::Config.app_id]
+    })
+  end
+
+  # Flags if the comment was imported
+  def imported?
+    !angel_list_id.blank?
   end
 end
