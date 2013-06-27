e5b8f4ffc4d081094285c77f4f334d9e8c051e50
Stas SUȘCOV <stas@net.utcluj.ro>
2013-06-27 12:27:46 +0300

    Installed delayed_job.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130624164233) do
+ActiveRecord::Schema.define(version: 20130627092613) do
 
   create_table "assets", force: true do |t|
     t.integer  "project_id"
@@ -77,6 +77,22 @@ ActiveRecord::Schema.define(version: 20130624164233) do
   add_index "comments", ["parent_comment_id"], name: "index_comments_on_parent_comment_id"
   add_index "comments", ["project_id"], name: "index_comments_on_project_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "identities", force: true do |t|
     t.string   "uid"
