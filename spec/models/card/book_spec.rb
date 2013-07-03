6be008853290584a7381eb2cf94f92afdd9f976f
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-03 13:31:27 +0300

    Added book card. Completes #6345286886889

---
+++ spec/models/card/book_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Card::Book do
+  it { should have_one(:image).dependent(:destroy) }
+  it { should validate_presence_of(:book_title) }
+  it { should validate_presence_of(:book_authors) }
+
+  context 'instance' do
+    subject(:book_card) { Fabricate('card/book') }
+
+    its(:image) { should_not be_blank }
+    its(:url) { should_not be_blank }
+  end
+end
