934e7130ad48bb9d69179d059a9b73e7f0fc56fa
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-03 11:33:02 +0300

    Added photo card. Completes #6345286886885

---
+++ spec/fabricators/card/photo_fabricator.rb
@@ -0,0 +1,9 @@
+Fabricator('card/photo') do
+  title { Faker::Lorem.sentence }
+  user
+  board
+  after_create do |photo_card, trans|
+    photo_card.image = Fabricate.build(:image, :user => photo_card.user,
+      :board => photo_card.board, :assetable => photo_card)
+  end
+end
