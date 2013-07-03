2e0c3e0806e9821ee4323584986967082a77d144
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-03 13:52:23 +0300

    Added video card. Completes #6345286886887

---
+++ app/models/card/video.rb
@@ -0,0 +1,14 @@
+# DOERS video [Card] model
+class Card::Video < Card
+  PROVIDERS = %w(youtube)
+
+  # Relationships
+  has_one :image, :dependent => :destroy, :foreign_key => :assetable_id
+
+  # Store accessors definition
+  store_accessor :data, :video_id, :provider
+
+  # Validations
+  validates_presence_of :video_id, :provider
+  validates_inclusion_of :provider, :in => PROVIDERS
+end
