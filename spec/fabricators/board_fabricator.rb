9a5d471211fd28fc2a855ff647b31db38c0cf5ca
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-05 19:04:36 +0300

    Branched board fabricator should set parent board status public.

--- spec/fabricators/board_fabricator.rb
+++ spec/fabricators/board_fabricator.rb
@@ -7,9 +7,13 @@ end
 Fabricator(:branched_board, :class_name => Board) do
   user
   project
-  parent_board(:fabricator => :board)
+  parent_board(:fabricator => :persona_board)
   title       { Faker::Lorem.sentence }
   description { Faker::Lorem.sentence }
+
+  after_create do |board|
+    board.parent_board.update_attributes(:status => Board::STATES.last)
+  end
 end
 
 Fabricator(:persona_board, :class_name => Board) do
