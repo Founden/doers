35a9018cdeef4c41a2b4b6b3e46b2c272c5147e8
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-05 12:13:06 +0300

    Added the description for board fabricators.

--- spec/fabricators/board_fabricator.rb
+++ spec/fabricators/board_fabricator.rb
@@ -1,26 +1,31 @@
 Fabricator(:board) do
   author(:fabricator => :user)
-  title { Faker::Lorem.sentence }
+  title       { Faker::Lorem.sentence }
+  description { Faker::Lorem.sentence }
 end
 
 Fabricator(:branched_board, :class_name => Board) do
   user
   project
   parent_board(:fabricator => :board)
-  title { Faker::Lorem.sentence }
+  title       { Faker::Lorem.sentence }
+  description { Faker::Lorem.sentence }
 end
 
 Fabricator(:persona_board, :class_name => Board) do
-  title { sequence(:persona_title){|t| 'Persona board #%d' % t} }
+  title       { sequence(:persona_title){|t| 'Persona board nr.%d' % t} }
+  description { Faker::Lorem.sentence }
   author(:fabricator => :user)
 end
 
 Fabricator(:problem_board, :class_name => Board) do
-  title { sequence(:problem_title){|t| 'Problem board #%d' % t} }
+  title       { sequence(:problem_title){|t| 'Problem board nr.%d' % t} }
+  description { Faker::Lorem.sentence }
   author(:fabricator => :user)
 end
 
 Fabricator(:solution_board, :class_name => Board) do
-  title { sequence(:solution_title){|t| 'Solution board #%d' % t} }
+  title       { sequence(:solution_title){|t| 'Solution board nr.%d' % t} }
+  description { Faker::Lorem.sentence }
   author(:fabricator => :user)
 end
