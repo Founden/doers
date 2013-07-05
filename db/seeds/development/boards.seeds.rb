be2a58c68fb2625a0b3aa11eef7a502a88fffc4b
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-05 11:24:53 +0300

    Persona, Problem and Solution board seeds should be public.

--- db/seeds/development/boards.seeds.rb
+++ db/seeds/development/boards.seeds.rb
@@ -2,9 +2,9 @@ user = User.find_by(:email => ENV['EMAIL'])
 
 after 'development:projects' do
   # Initial boards
-  persona = Fabricate(:persona_board)
-  problem = Fabricate(:problem_board)
-  solution = Fabricate(:solution_board)
+  persona = Fabricate(:persona_board, :status => Board::STATES.last)
+  problem = Fabricate(:problem_board, :status => Board::STATES.last)
+  solution = Fabricate(:solution_board, :status => Board::STATES.last)
   # My boards
   user.projects.limit(2).each do |project|
     Fabricate(:branched_board, :user => user, :parent_board => persona,
