21995fac8746f540ed805f097d22eb7097410d1c
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-05 16:04:13 +0300

    Added JSON to OpenStruct rspec helper to be used in controllers

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -7,6 +7,18 @@ module Doers::RSpecHelpers
     # TODO: Remove this once we do not need confirmations anymore
     User.last.update_attribute(:confirmed, true)
   end
+
+  # Parses a JSON into an OpenStruct data set
+  # Remember, nested groups are not parsed, so [Hash] access is still needed
+  #
+  # @param [String] string, the JSON content
+  # @param [Symbol] root, the JSON root to use
+  def json_to_ostruct(string, root=nil)
+    json = ActiveSupport::JSON.decode(string)
+    ostruct = root ? OpenStruct.new(json[root.to_s]) : OpenStruct.new(json)
+    ostruct.keys = root ? json[root.to_s].keys : json.keys
+    ostruct
+  end
 end
 
 RSpec.configure do |config|
