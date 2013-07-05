bdd633871c0284d2330e2cbbe70613076be012be
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-05 12:16:47 +0300

    Updated board serializer.

--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -1,12 +1,28 @@
 # [Board] model serializer
 class BoardSerializer < ActiveModel::Serializer
   attributes :id, :title, :status, :updated_at, :description
+  attributes :last_update, :author_nicename, :user_nicename
 
-  has_one :user, :embed => :ids
-  has_one :author, :embed => :ids
-  has_one :project, :embed => :ids
-  has_one :parent_board, :embed => :ids
+  has_one :user, :embed => :id
+  has_one :author, :embed => :id
+  has_one :project, :embed => :id
+  has_one :parent_board, :embed => :id
 
   has_many :branches, :embed => :ids
   has_many :cards, :embed => :ids
+
+  # Creates a nice timestamp to indicate when it was last time updated
+  def last_update
+    object.updated_at.to_s(:pretty)
+  end
+
+  # If this card has an author, show the author nicename
+  def author_nicename
+    object.author.nicename if object.author
+  end
+
+  # If this card has a user, show the user nicename
+  def user_nicename
+    object.user.nicename if object.user
+  end
 end
