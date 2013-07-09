290e8df45aa8ad41b9b5c51bb611f47d0a1c44e8
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-09 18:03:08 +0300

    Added project relationship to cards.

--- spec/fabricators/card/map_fabricator.rb
+++ spec/fabricators/card/map_fabricator.rb
@@ -1,7 +1,8 @@
 Fabricator('card/map') do
-  location { Faker::Lorem.sentence }
-  address  { '23 Emil Isac str., Cluj-Napoca, Romania' }
-  title { Faker::Lorem.sentence }
+  location  { Faker::Lorem.sentence }
+  address   { '23 Emil Isac str., Cluj-Napoca, Romania' }
+  title     { Faker::Lorem.sentence }
   user
   board
+  project   { nil }
 end
--- spec/fabricators/card/photo_fabricator.rb
+++ spec/fabricators/card/photo_fabricator.rb
@@ -1,7 +1,8 @@
 Fabricator('card/photo') do
-  title { Faker::Lorem.sentence }
+  title   { Faker::Lorem.sentence }
   user
   board
+  project { nil }
   after_create do |photo_card, trans|
     photo_card.image = Fabricate.build(:image, :user => photo_card.user,
       :board => photo_card.board, :assetable => photo_card)
