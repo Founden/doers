d45b3ee24d66ed058c89db0ec4153bc295e47948
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-18 20:11:53 +0300

    Added assets backend. #6592974000112

---
+++ app/serializers/asset_serializer.rb
@@ -0,0 +1,20 @@
+# [Asset] model serializer
+class AssetSerializer < ActiveModel::Serializer
+  attributes :id, :description, :updated_at, :type
+  attributes :attachment_url, :user_nicename
+
+  has_one :user, :embed => :id
+  has_one :project, :embed => :id
+  has_one :board, :embed => :id
+
+  # If this card has a user, show the user nicename
+  def user_nicename
+    object.user.nicename if object.user
+  end
+
+  # Attachment URL helper
+  def attachment_url
+    object.attachment.url if object.attachment
+  end
+end
+
