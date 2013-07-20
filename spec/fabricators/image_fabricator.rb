55a4c78b8b5a600a82859b3485a31dddd1b2c281
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-20 19:19:04 +0300

    Added tests for assets backend #6592974000112

--- spec/fabricators/image_fabricator.rb
+++ spec/fabricators/image_fabricator.rb
@@ -2,3 +2,14 @@ Fabricator(:image) do
   description { sequence(:description) { Faker::Lorem.paragraph } }
   attachment  { File.open(Rails.root.join('spec/fixtures/test.png')) }
 end
+
+Fabricator(:image_to_upload, :from => :image) do
+  user
+  project
+  board
+  assetable_type { nil }
+  assetable_id   { nil }
+  type           { Image }
+  attachment     { Rack::Test::UploadedFile.new(
+    Rails.root.join('spec/fixtures/test.png'), 'image/png') }
+end
