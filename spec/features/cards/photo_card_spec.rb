d0a61c202aee195da24a858f5c8eba6f5fb4e78a
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-24 15:18:13 +0300

    Updated photo card feature test.

--- spec/features/cards/photo_card_spec.rb
+++ spec/features/cards/photo_card_spec.rb
@@ -26,6 +26,19 @@ feature 'Photo', :js, :vcr => {:cassette_name=>:angel_list_oauth2} do
       expect(page).to have_content(card.title)
       expect(page.source).to include(card.image.attachment.url)
     end
+
+    scenario 'updates image when dropping an image' do
+      file = Rails.root.join('spec/fixtures/test.png')
+      card_name = 'card-%s-%d' % [card.class.name.demodulize.downcase, card.id]
+
+      old_image_url = card.image.attachment.url.force_encoding('UTF-8')
+      page.attach_file(card_name, file)
+
+      # Let it ajax
+      sleep(0.5)
+      expect(page.source).to_not include(old_image_url)
+      expect(page.source).to match('data:image/png;base64')
+    end
   end
 
 end
