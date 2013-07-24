5186416e90416c99cc96656dd7e0d17d996a82a6
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-24 15:58:39 +0300

    Split off projects feature test in the ones in need of puffing_billy and not.

---
+++ spec/features/projects_import_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+feature 'Projects', :js, :no_ci, :vcr => {:cassette_name=>:angel_list_oauth2} do
+  background do
+    sign_in_with_angel_list
+  end
+
+  context 'user with Angel List startups' do
+    given(:startups) do
+      MultiJson.load(Rails.root.join('spec/fixtures/angel_list_startups.json'))
+    end
+    given(:startup) { startups['startup_roles'][3]['startup'] }
+
+    background do
+      Delayed::Job.stub(:enqueue)
+      proxy.stub(/startup_roles/).and_return(:jsonp => startups)
+    end
+
+    context 'on import screen' do
+      background do
+        visit root_path(:anchor => :dashboard)
+        click_on('projects-import')
+      end
+
+      scenario 'are listed' do
+        expect(page).to have_css('.projects .project', :count => 2)
+      end
+
+      scenario 'after import shows importer running screen' do
+        # Pick 3rd startup where role is `founder`
+        find('.startup-%d' % startup['id']).click
+        expect(page).to have_css('.projects .project.selected', :count => 1)
+
+        click_on('run-import')
+
+        expect(page).to_not have_css('#importer')
+        expect(page).to have_css('#importer-running')
+      end
+    end
+  end
+
+  context 'user with no Angel List startups' do
+    background do
+      proxy.stub(/startup_roles/).and_return(:jsonp => [])
+    end
+
+    context 'on import screen' do
+      background do
+        visit root_path(:anchor => :dashboard)
+        click_on('projects-import')
+      end
+
+      scenario 'none are listed' do
+        expect(page).to have_css('.projects .project', :count => 0)
+      end
+    end
+  end
+end
