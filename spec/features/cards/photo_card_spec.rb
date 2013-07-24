82294d4fb28e123ed3cbad8cf63d442b6d8054d8
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-24 09:42:33 +0300

    Feature specs for all our cards. #6985199284246

---
+++ spec/features/cards/photo_card_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+feature 'Photo', :js, :vcr => {:cassette_name=>:angel_list_oauth2} do
+  background do
+    sign_in_with_angel_list
+  end
+
+  context 'card from an existing project board' do
+    given(:project) do
+      Fabricate(:project_with_boards_and_cards,
+                :user => User.first, :card_types => %w(card/photo))
+    end
+    given(:board) { project.boards.first }
+    given(:card) { board.cards.first }
+
+    background do
+      visit root_path(:anchor=>'projects/%d/boards/%d' % [project.id, board.id])
+    end
+
+    scenario 'is shown with details' do
+      expect(page).to have_css('.cards .card', :count => 1)
+
+      card_classname = '.cards .%s' % card.class.name.demodulize.downcase
+      expect(page).to have_css(card_classname)
+
+      expect(page).to have_content(card.title)
+      expect(page.source).to include(card.image.attachment.url)
+    end
+  end
+
+end
