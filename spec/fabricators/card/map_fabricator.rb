50508c494779e40fb925cc60fb6561219b9590cd
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-25 23:58:25 +0300

    Fix bigdecimal issue with lat/lon test.

--- spec/fabricators/card/map_fabricator.rb
+++ spec/fabricators/card/map_fabricator.rb
@@ -2,8 +2,8 @@ Fabricator('card/map') do
   content   { sequence(:addres) { '%s, %s, %s' % [
     Faker::Address.street_address,Faker::Address.city,Faker::Address.country] }}
   title     { sequence(:location) { Faker::Address.neighborhood } }
-  latitude  { Faker::Geolocation.lat }
-  longitude { Faker::Geolocation.lng }
+  latitude  { Faker::Geolocation.lat.round(10) }
+  longitude { Faker::Geolocation.lng.round(10) }
   user
   board
   project   { nil }
