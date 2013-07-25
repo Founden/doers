ae922671c73129849eb54daa831c8a8db4f966e3
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-25 19:37:36 +0300

    Remove external_type from users. Cleanup tests. User identity is more appropriate.

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -2,7 +2,6 @@ Fabricator(:user) do
   name                    { sequence(:name) { |nid| Faker::Name.name } }
   email                   { sequence(:email) { |eid| Faker::Internet.email } }
   external_id             { sequence(:external_id) }
-  external_type           { Doers::Config.external_types.first }
   confirmed               { true }
   identities(:count => 1) { |attrs, i|
     Fabricate(:angel_list_identity, :uid => attrs[:email]) }
