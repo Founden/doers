6c8beb1b8dd13e9938e430d9e45e581e53186e76
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-25 14:25:39 +0300

    More attribute cleanups. Added indexes to the tables.

--- db/migrate/20130409083321_create_users.rb
+++ db/migrate/20130409083321_create_users.rb
@@ -1,14 +1,16 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.integer :external_id, :limit => 8
       t.string :name
       t.string :email
       t.hstore :data
+      t.string :external_id
+      t.string :external_type
 
       t.timestamps
     end
 
     add_index :users, :email
+    add_index :users, [:external_id, :external_type]
   end
 end
