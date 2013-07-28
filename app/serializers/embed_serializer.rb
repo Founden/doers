3c8aa3a924c5ad0a0aa980bf816da936603ff470
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-28 18:01:00 +0300

    Added embeds controller and serializer.

---
+++ app/serializers/embed_serializer.rb
@@ -0,0 +1,20 @@
+# [Faraday::Response] object serializer
+class EmbedSerializer < ActiveModel::Serializer
+  root :embed
+
+  attributes :title, :height, :width, :html, :type => :embed_type
+  attributes :provider_name, :thumbnail_url, :author_url, :author_name
+
+  # Overwrite serializer constructor
+  def initialize(object, options={})
+    object = OpenStruct.new object.body
+    # Make [ActiveModel::Serializer] accept a [Faraday::Response] object
+    object.send(:extend, ActiveModel::SerializerSupport)
+    super(object, options)
+  end
+
+  # Underscore provider name to have a format for it
+  def provider_name
+    object.provider_name.to_s.underscore
+  end
+end
