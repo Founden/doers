d455752fae918c7b724205f489f8f73b16ca85a1
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-29 14:45:46 +0300

    Link card url is now content, so we don't cache embed responses.

--- spec/fabricators/card/link_fabricator.rb
+++ spec/fabricators/card/link_fabricator.rb
@@ -1,6 +1,5 @@
 Fabricator('card/link') do
-  url     { Faker::Internet.uri(Card::Link::ALLOWED_SCHEMES.sample) }
-  content { Faker::Lorem.paragraph }
+  content { Faker::Internet.uri(Card::Link::ALLOWED_SCHEMES.sample) }
   title   { Faker::Lorem.sentence }
   user
   board
