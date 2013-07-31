cb4bff2f4b701eb210a8287986a1f3f7f2aaad2d
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-31 13:51:20 +0300

    Updated oembedr to use github repo.

--- app/controllers/api/v1/embeds_controller.rb
+++ app/controllers/api/v1/embeds_controller.rb
@@ -3,7 +3,10 @@ class Api::V1::EmbedsController < Api::V1::ApplicationController
 
   # Fetches oEmbed link
   def index
-    if Oembedr.known_service?(url) and (embed = Oembedr.fetch(url))
+    # Oembedr params
+    params = { :params => {:nowrap => 'on', :for => Doers::Config.app_id} }
+
+    if Oembedr.known_service?(url) and (embed = Oembedr.fetch(url, params))
       render :json => [embed], :each_serializer => EmbedSerializer
     else
       render :nothing => true, :status => 400
