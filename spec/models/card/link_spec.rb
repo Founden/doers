d1435108ad388f2d9aad0e4be7ee9deb17601608
Stas SUȘCOV <stas@net.utcluj.ro>
2013-07-31 10:31:07 +0300

    Revert "Link card url is now content, so we don't cache embed responses."
    This reverts commit d455752fae918c7b724205f489f8f73b16ca85a1.
    
    Conflicts:
    	spec/controllers/api/v1/cards_controller_spec.rb

--- spec/models/card/link_spec.rb
+++ spec/models/card/link_spec.rb
@@ -4,12 +4,13 @@ describe Card::Link do
   let(:link_card) { Fabricate('card/link') }
 
   it { should validate_presence_of(:content) }
-  it { should allow_value(Faker::Internet.uri(:http)).for(:content) }
-  it { should allow_value(Faker::Internet.uri(:https)).for(:content) }
+  it { should allow_value(Faker::Internet.uri(:http)).for(:url) }
+  it { should allow_value(Faker::Internet.uri(:https)).for(:url) }
+  it { should_not allow_value(Faker::Lorem.sentence).for(:url) }
 
   context 'instance' do
     subject { link_card }
 
-    its(:content) { should_not be_blank }
+    its(:url) { should_not be_blank }
   end
 end
