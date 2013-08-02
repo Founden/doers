cc8573252205a169a732fc0d321185e844e5f21b
Stas SUȘCOV <stas@net.utcluj.ro>
2013-08-02 19:29:50 +0300

    Added phrase card edit feature test.

--- spec/features/cards/phrase_card_spec.rb
+++ spec/features/cards/phrase_card_spec.rb
@@ -20,11 +20,34 @@ feature 'Phrase', :js, :slow, :vcr do
     scenario 'is shown with details' do
       expect(page).to have_css('.cards .card', :count => 1)
 
-      card_classname = '.cards .%s' % card.class.name.demodulize.downcase
-      expect(page).to have_css(card_classname)
+      expect(page).to have_css('.card-%d' % card.id)
 
       expect(page).to have_content(card.title)
       expect(page).to have_content(card.content)
+    end
+
+    context 'when clicked on edit' do
+      let(:title) { Faker::Lorem.sentence }
+      let(:content) { Faker::Lorem.sentence }
+
+      background do
+        page.find('.card-%d .card-settings' % card.id).click
+        page.find('#dropdown-card-%d .toggle-editing' % card.id).click
+      end
+
+      scenario 'can edit card details in editing screen' do
+        edit_css = '#edit-card-%d' % card.id
+
+        within(edit_css) do
+          fill_in('title', :with => title)
+          fill_in('content', :with => content)
+        end
+        page.find(edit_css + ' .actions .button').click
+
+        expect(page).to_not have_css(edit_css)
+        expect(page).to have_content(title)
+        expect(page).to have_content(content)
+      end
     end
   end
 
