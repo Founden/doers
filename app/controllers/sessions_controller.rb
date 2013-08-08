11526ad6cbfddf67f14f6bd1abc7eb2438e4bcce
Stas SUȘCOV <stas@net.utcluj.ro>
2013-08-08 14:44:50 +0300

    Handle unauthorized signup Closes #68 #7193027481003

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,14 @@ class SessionsController < ApplicationController
   end
 
   private
+  # Returns location to redirect after signing in failure
+  def after_failed_sign_in
+    flash[:error] = _('Something went wrong. Please try again.')
+    redirect_to root_path
+  end
 
   # Returns location to redirect after signing in
-  def after_successful_sign_in_url_default
-    profile_path(current_user)
+  def after_successful_sign_in_url
+    profile_path(current_account)
   end
 end
