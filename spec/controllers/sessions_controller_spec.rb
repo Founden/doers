8aed32a9643afe991efd509f23ce2eca73a2a356
Stas SUȘCOV <stas@net.utcluj.ro>
2013-08-08 14:44:50 +0300

    Handle unauthorized signup Closes #68 #7193027481003

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -5,11 +5,11 @@ describe SessionsController do
   describe 'private methods' do
     let(:user_attr) { Fabricate.build(:user, :id => 1) }
     before do
-      controller.should_receive(:current_user).and_return(user_attr)
+      controller.should_receive(:current_account).and_return(user_attr)
     end
 
     it 'returns after redirect location' do
-      controller.send(:after_successful_sign_in_url_default).should eq(
+      controller.send(:after_successful_sign_in_url).should eq(
         profile_path(user_attr))
     end
 
@@ -23,4 +23,27 @@ describe SessionsController do
     it { should render_template(:index) }
   end
 
+  describe '#create' do
+    let(:params) {  }
+    before { get(:create, params) }
+
+    context 'for denied authentication' do
+      let(:params) do
+        {:error => 'access_denied', :identity => :oauth2,
+         :provider => 'angel_list'}
+      end
+
+      it { should redirect_to(root_path) }
+      its('flash.keys') { should include(:error) }
+    end
+
+    context 'for allowed authentication' do
+      let(:params) do
+        {:provider => :angel_list, :identity => :oauth2, :code => 'DUMMY_CODE'}
+      end
+
+      it { should redirect_to(profile_path(User.first)) }
+      its('flash.keys') { should include(:notice) }
+    end
+  end
 end
