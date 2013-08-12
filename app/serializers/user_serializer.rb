ccdb437b537f8266f44b7c05995a708d639e4621
Stas SUȘCOV <stas@net.utcluj.ro>
2013-08-12 19:59:59 +0300

    Serialize just a subset of user attributes if user is now allowed to get those.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -15,4 +15,11 @@ class UserSerializer < ActiveModel::Serializer
   def avatar_url
     gravatar_uri(object.email)
   end
+
+  # Method to use for aliasing what attribtues can be included
+  def is_current_user?
+    current_account.id.eql?(object.id)
+  end
+  alias_method :include_external_id?, :is_current_user?
+  alias_method :include_angel_list_token?, :is_current_user?
 end
