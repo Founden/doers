059f11b1a6fc883c0288b3a363c1fe140373d466
Stas SUȘCOV <stas@net.utcluj.ro>
2013-08-12 19:59:59 +0300

    Serialize just a subset of user attributes if user is now allowed to get those.

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -18,17 +18,31 @@ describe Api::V1::UsersController do
   describe '#show' do
     include GravatarHelper
 
-    before { get(:show, :id => user.id) }
+    let(:user_id) { user.id }
+
+    before { get(:show, :id => user_id) }
 
     subject(:api_user) { json_to_ostruct(response.body, :user) }
 
-    its('keys.size') { should eq(7) }
-    its(:id) { should eq(user.id) }
-    its(:nicename) { should eq(user.nicename) }
-    its(:external_id) { should eq(user.external_id) }
-    its(:angel_list_token) { should eq(user.identities.first.token) }
-    its(:isImporting) { should be_false }
-    its(:avatar_url) { should eq(gravatar_uri(user.email)) }
-    its(:is_admin) { should eq(user.admin?) }
+    context 'shows all details if user owns the profile' do
+      its('keys.size') { should eq(6) }
+      its(:id) { should eq(user.id) }
+      its(:nicename) { should eq(user.nicename) }
+      its(:external_id) { should eq(user.external_id.to_s) }
+      its(:angel_list_token) { should eq(user.identities.first.token) }
+      its(:importing) { should be_false }
+      its(:avatar_url) { should eq(gravatar_uri(user.email)) }
+    end
+
+    context 'shows limited details for foreign profile' do
+      let(:some_user) { Fabricate(:user) }
+      let(:user_id) { some_user.id }
+
+      its('keys.size') { should eq(4) }
+      its(:id) { should eq(some_user.id) }
+      its(:nicename) { should eq(some_user.nicename) }
+      its(:importing) { should be_false }
+      its(:avatar_url) { should eq(gravatar_uri(some_user.email)) }
+    end
   end
 end
