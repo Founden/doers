4dcfaa75f0c71dff3abf769fdfcee3dac785621e
Stas SUȘCOV <stas@net.utcluj.ro>
2013-08-12 20:59:57 +0300

    On wrong profile ids, return current user so we can use `/users/(mine|me)`

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -24,21 +24,31 @@ describe Api::V1::UsersController do
 
     subject(:api_user) { json_to_ostruct(response.body, :user) }
 
-    context 'shows all details if user owns the profile' do
-      its('keys.size') { should eq(6) }
+    shared_examples 'shows current user details' do
+      its('keys.size') { should eq(7) }
       its(:id) { should eq(user.id) }
       its(:nicename) { should eq(user.nicename) }
-      its(:external_id) { should eq(user.external_id.to_s) }
+      its('external_id.to_i') { should eq(user.external_id) }
       its(:angel_list_token) { should eq(user.identities.first.token) }
-      its(:importing) { should be_false }
+      its(:is_importing) { should be_false }
+      its(:is_admin) { should eq(user.admin?) }
       its(:avatar_url) { should eq(gravatar_uri(user.email)) }
+    end
+
+    context 'if user owns the profile' do
+      it_should_behave_like 'shows current user details'
+    end
+
+    context 'if profile id is not found' do
+      let(:user_id) { 'mine' }
+      it_should_behave_like 'shows current user details'
     end
 
     context 'shows limited details for foreign profile' do
       let(:some_user) { Fabricate(:user) }
       let(:user_id) { some_user.id }
 
-      its('keys.size') { should eq(4) }
+      its('keys.size') { should eq(5) }
       its(:id) { should eq(some_user.id) }
       its(:nicename) { should eq(some_user.nicename) }
       its(:importing) { should be_false }
