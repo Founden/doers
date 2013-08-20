cf55050c21dfcd01dbeb9b0a3e98352b22e47e5c
Stas SUȘCOV <stas@net.utcluj.ro>
2013-08-20 20:40:18 +0300

    Updating a card image should trigger a custom activity on parent card. #7195499091411

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,4 +3,10 @@ class Image < Asset
   # Validations
   validates_presence_of :user, :board, :assetable
   validates_attachment_content_type :attachment, :content_type => IMAGE_TYPES
+
+  # Callbacks
+  after_create do
+    self.assetable.send(:generate_activity, Image.name) if (
+      self.assetable.respond_to?(:generate_activity, true))
+  end
 end
