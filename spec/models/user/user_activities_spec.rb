886592bf37a358d1bea96da9d2c70da08a8a7694
Stas SUȘCOV <stas@net.utcluj.ro>
2013-08-20 17:11:38 +0300

    Added support for acitvities to use class.

---
+++ spec/models/user/user_activities_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe User, :use_truncation do
+  let!(:user) { Fabricate(:user) }
+
+  context '#activities' do
+    subject { user.activities }
+
+    context 'on create' do
+      its(:size) { should eq(1) }
+      its('first.user') { should eq(user) }
+      its('first.trackable') { should eq(user) }
+      its('first.slug') { should match('create-user') }
+    end
+
+    context 'on update' do
+      before { user.update_attributes(:name => Faker::Name.name) }
+
+      its(:size) { should eq(1) }
+    end
+
+    context 'on delete' do
+      before { user.destroy }
+
+      its(:size) { should eq(1) }
+    end
+  end
+end
