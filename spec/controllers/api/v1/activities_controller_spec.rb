6cdba098d5678abbe76b1eeb04fa2e9919eed590
Stas SUȘCOV <stas@net.utcluj.ro>
2013-08-21 18:50:24 +0300

    Added activities controller and serializer.

---
+++ spec/controllers/api/v1/activities_controller_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+describe Api::V1::ActivitiesController, :use_truncation do
+  let(:user) { Fabricate(:user) }
+
+  before do
+    controller.stub(:current_account) { user }
+  end
+
+  describe '#index' do
+    let(:ids) { user.activities.pluck('id') }
+
+    before { get(:index, :ids => ids) }
+
+    subject(:api_user) { json_to_ostruct(response.body) }
+
+    its('activities.count') { should eq(1) }
+  end
+
+  describe '#show' do
+    let(:activity_id) { activity.id }
+    let(:activity) { user.activities.first }
+
+    before { get(:show, :id => activity_id) }
+
+    subject(:api_activity) { json_to_ostruct(response.body, :activity) }
+
+    its('keys.size') { should eq(13) }
+    its(:id) { should eq(activity.id) }
+    its(:slug) { should eq(activity.slug) }
+    its(:user_id) { should eq(user.id) }
+    its(:project_id) { should be_nil }
+    its(:board_id) { should be_nil }
+    its(:trackable_id) { should eq(user.id) }
+    its(:trackable_type) { should eq(user.class.name) }
+    its(:user_name) { should eq(user.nicename) }
+    its(:project_title) { should be_nil }
+    its(:board_title) { should be_nil }
+    its(:trackable_title) { should be_nil }
+    its(:updated_at) { should_not be_blank }
+    its(:last_update) { should eq(activity.updated_at.to_s(:pretty)) }
+  end
+
+  describe '#destroy' do
+    let(:activity) { user.activities.first }
+    let(:activity_id) { activity.id }
+
+    before { delete(:destroy, :id => activity_id) }
+
+    its('response.status') { should eq(204) }
+    its('response.body') { should be_blank }
+
+    context 'board is not owned by current user' do
+      let(:activity_id) { Fabricate(:user).activities.first.id }
+
+      its('response.status') { should eq(400) }
+    end
+  end
+end
