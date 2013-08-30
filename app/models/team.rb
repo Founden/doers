c739cacb5c92a66391bc0ef3c1a2aa3607953c1d
Stas SUȘCOV <stas@net.utcluj.ro>
2013-08-30 16:24:10 +0300

    Updated team model validations to include website and angel_list

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,22 +1,27 @@
 # Doers [User] Team class
 class Team < ActiveRecord::Base
-  store_accessor :data, :website, :angeli_list
+  # List of allowed URI schemes
+  ALLOWED_SCHEMES = %w(http https)
+
+  store_accessor :data, :website, :angel_list
 
   # Relationships
   has_many :boards
-  has_many :users, :through => :boards
+  has_many :users, :through => :boards, :source => :author
   has_one(:banner, :as => :assetable,
           :class_name => Asset::Banner, :dependent => :destroy)
 
   # Validations
   validates_presence_of :title, :slug
   validates_uniqueness_of :slug
+  validates_format_of :website, :with => URI.regexp(ALLOWED_SCHEMES)
+  validates_format_of :angel_list, :with => URI.regexp(ALLOWED_SCHEMES)
 
   # Callbacks
   before_validation do
     # Sanitize user input
     self.title = Sanitize.clean(self.title)
     self.description = Sanitize.clean(self.description)
-    self.slug = (Sanitize.clean(self.slug) || self.title).parameterize
+    self.slug = (Sanitize.clean(self.slug) || self.title).to_s.parameterize
   end
 end
