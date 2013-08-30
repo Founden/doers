58dd82921788c556ddd57197845f62d5ae7e9804
Stas SUȘCOV <stas@net.utcluj.ro>
2013-08-30 12:32:44 +0300

    Added teams. Boards can represent a team!

---
+++ spec/fabricators/team_fabricator.rb
@@ -0,0 +1,9 @@
+Fabricator(:team) do
+  title       Faker::Company.name
+  description Faker::Company.catch_phrase
+  website     Faker::Internet.http_url
+  after_create do |team, trans|
+    team.banner = Fabricate(:banner, :assetable => team)
+    2.times { Fabricate(:board, :team => team) }
+  end
+end
---
+++ spec/models/team_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe Team do
+  it { should have_one(:banner).dependent(:destroy) }
+  it { should have_many(:boards) }
+  it { should have_many(:users).through(:boards) }
+
+  context 'instance' do
+    subject(:team) { Fabricate(:team) }
+
+    context 'sanitizes' do
+      let(:content) { Faker::HTMLIpsum.body[0..250] }
+
+      context '#title' do
+        before { team.update_attributes(:title => content) }
+
+        its(:title) { should eq(Sanitize.clean(content)) }
+        its(:slug) { should_not eq(team.title.parameterize) }
+      end
+
+      context '#description' do
+        before { team.update_attributes(:description => content) }
+
+        its(:description) { should eq(Sanitize.clean(content)) }
+      end
+
+      context '#slug' do
+        subject(:team) { Fabricate(:team, :title => content) }
+
+        its(:slug) { should eq(team.title.parameterize) }
+
+        context 'on update' do
+          let(:new_content) { Faker::HTMLIpsum.body[0..250] }
+          before { team.update_attributes(:slug => new_content) }
+
+          its(:slug) { should eq(Sanitize.clean(new_content).parameterize) }
+        end
+      end
+    end
+  end
+end
