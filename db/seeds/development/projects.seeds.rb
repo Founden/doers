6b3ce6a9102a5999f369e500ec8ec9dafa778938
Stas SUȘCOV <stas@net.utcluj.ro>
2013-09-02 18:04:30 +0300

    Updated fabricators and seeds to include invitations.

--- db/seeds/development/projects.seeds.rb
+++ db/seeds/development/projects.seeds.rb
@@ -2,7 +2,7 @@ user = User.find_by(:email => ENV['EMAIL'])
 
 after 'development:users' do
   # My projects
-  5.times { Fabricate(:project, :user => user) }
+  5.times { Fabricate(:project_with_invitations, :user => user) }
   # Other projects
   15.times { Fabricate(:project) }
 end
