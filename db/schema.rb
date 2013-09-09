c8f385b82db081508944468485990c2377ccac3c
Stas SUȘCOV <stas@net.utcluj.ro>
2013-09-09 23:23:39 +0300

    Upgraded gutentag gem migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130827202659) do
+ActiveRecord::Schema.define(version: 20130909202228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -186,12 +186,14 @@ ActiveRecord::Schema.define(version: 20130827202659) do
   add_index "taggings", ["taggable_type", "taggable_id"], name: "index_taggings_on_taggable_type_and_taggable_id", using: :btree
 
   create_table "tags", force: true do |t|
-    t.string   "name",       null: false
+    t.string   "name",                       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "taggings_count", default: 0, null: false
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
+  add_index "tags", ["taggings_count"], name: "index_tags_on_taggings_count", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name"
