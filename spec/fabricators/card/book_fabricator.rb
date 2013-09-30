021f878a74169e754c20eecf1cc0b058ec13ef5f
Stas SUȘCOV <stas@net.utcluj.ro>
2013-09-30 19:33:52 +0300

    Update cards fabricators topic.

--- spec/fabricators/card/book_fabricator.rb
+++ spec/fabricators/card/book_fabricator.rb
@@ -5,8 +5,8 @@ Fabricator('card/book') do
   project    { |attrs| Fabricate(:project, :user => attrs[:user]) }
   board      { |attrs| Fabricate(
     :branched_board, :user => attrs[:user], :project => attrs[:project]) }
-  topic      { |attrs|
-    Fabricate(:topic, :user => attrs[:user], :board => attrs[:board]) }
+  topic      { |attrs| Fabricate(
+    :topic, :user => attrs[:user], :board => attrs[:board].parent_board) }
 
   book_title   { Faker::Lorem.sentence }
   url          { Faker::Internet.http_url }
