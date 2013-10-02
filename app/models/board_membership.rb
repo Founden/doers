6b914dfc9d686eaf613ca92593ff67ced6ebe764
Stas SUȘCOV <stas@net.utcluj.ro>
2013-10-02 13:03:39 +0300

    I have no idea what is this :horse:

--- app/models/board_membership.rb
+++ app/models/board_membership.rb
@@ -1,8 +1,5 @@
 # DOERS [Membership] class for board collaborators
 class BoardMembership < Membership
-  # Relationships
-  has_many :invitations, :as => :membership
-
   # Validations
   validates_presence_of :board
   validates_exclusion_of :board_id, :in => :user_board_ids
--- app/models/project_membership.rb
+++ app/models/project_membership.rb
@@ -1,8 +1,5 @@
 # DOERS [Membership] class for project collaborators
 class ProjectMembership < Membership
-  # Relationships
-  has_many :invitations, :as => :membership
-
   # Validations
   validates_presence_of :project
   validates_exclusion_of :project_id, :in => :user_project_ids
