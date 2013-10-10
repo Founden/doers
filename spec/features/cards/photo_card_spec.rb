6610994b4ed8ff3ba780e0c7d296a8f4c4599cec
Stefan Gugurel <stefangugurel@gmail.com>
2013-10-10 14:55:09 +0300

    Updated cards feature tests.

--- spec/features/cards/photo_card_spec.rb
+++ spec/features/cards/photo_card_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
-feature 'Photo', :js, :focus do
+feature 'Photo', :js, :slow do
   background do
     sign_in_with_angel_list
   end
 
   context 'card from an existing topic' do
     given(:card) { Fabricate('card/photo', :user => User.first) }
+    given(:topic) { card.topic }
 
     background do
       visit root_path(:anchor => '/board/%d/topic/%d' % [card.board.id, card.topic.id])
@@ -14,13 +15,13 @@ feature 'Photo', :js, :focus do
 
     scenario 'is shown with details' do
       expect(page).to have_css('.card', :count => 1)
-      expect(page.soruce).to include(card.title)
-      expect(page.source).to include(card.content)
+      expect(page.source).to include(card.title)
       expect(page.source).to include(card.image.attachment.url)
     end
 
     context 'when edited' do
       given(:title) { Faker::Lorem.sentence }
+      given(:content) { Faker::Lorem.sentence }
       given(:image_path) { Rails.root.join('spec/fixtures/test.png') }
 
       scenario 'can be saved' do
@@ -28,18 +29,28 @@ feature 'Photo', :js, :focus do
 
         within('.card-edit') do
           fill_in('title', :with => title)
+          fill_in('content', :with => content)
           attach_file('image', image_path)
         end
+        sleep(1)
         page.find('.save-card').click
 
         sleep(1)
         card.reload
         expect(card.title).to eq(title)
+        expect(card.content).to eq(content)
 
         expect(page.source).to_not include(old_image_url)
         expect(page.source).to match('data:image/png;base64')
       end
-    end
 
+      scenario 'can be deleted' do
+        page.find('.delete-card').click
+        expect(page).to_not have_css('.card')
+        sleep(1)
+        topic.reload
+        expect(topic.cards.count).to eq(0)
+      end
+    end
   end
 end
