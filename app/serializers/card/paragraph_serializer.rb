0481c2fed6a9d14dcd9dd76de85c071f9e2038f9
Stas SUȘCOV <stas@net.utcluj.ro>
2013-10-11 16:18:54 +0300

    Use polymorphism in order to fetch cards.

--- app/serializers/card/paragraph_serializer.rb
+++ app/serializers/card/paragraph_serializer.rb
@@ -1,8 +1,4 @@
 # [Card::Paragraph] model serializer
 class Card::ParagraphSerializer < CardSerializer
-  attributes :_paragraph
-
-  # Dummy attribute to make happy ember data
-  def _paragraph
-  end
+  root :paragraph
 end
--- app/serializers/card/photo_serializer.rb
+++ app/serializers/card/photo_serializer.rb
@@ -1,9 +1,5 @@
 # [Card::Photo] model serializer
 class Card::PhotoSerializer < CardSerializer
-  attributes :_photo
+  root :photo
   has_one :image, :embed => :id
-
-  # Dummy attribute to make happy ember data
-  def _photo
-  end
 end
