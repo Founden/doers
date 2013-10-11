5e84a52426f2eb889396448c7f7bc179e3e4c52d
Stas SUȘCOV <stas@net.utcluj.ro>
2013-10-11 11:34:42 +0300

    Added support for STI cards.

--- app/serializers/card/link_serializer.rb
+++ app/serializers/card/link_serializer.rb
@@ -1,4 +1,8 @@
 # [Card::Link] model serializer
 class Card::LinkSerializer < CardSerializer
-  attributes :url, :content
+  attributes :url, :content, :_link
+
+  # Dummy attribute to make happy ember data
+  def _link
+  end
 end
