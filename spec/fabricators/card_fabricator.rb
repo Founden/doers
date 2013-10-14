1a79c736065a49524af5487bbc050767f82e1499
Stas SUȘCOV <stas@net.utcluj.ro>
2013-10-14 19:29:34 +0300

    Cleanup some card fabricators.

--- spec/fabricators/card_fabricator.rb
+++ spec/fabricators/card_fabricator.rb
@@ -2,7 +2,6 @@ Fabricator(:card) do
   style      { Card::STYLES.sample }
   title      { Faker::Lorem.sentence }
   user
-  aligned    { false }
   project    { |attrs| Fabricate(:project, :user => attrs[:user]) }
   board      { |attrs| Fabricate(
     :branched_board, :user => attrs[:user], :project => attrs[:project]) }
