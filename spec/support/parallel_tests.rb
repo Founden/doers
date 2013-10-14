54e03aca5cbc56428c0046f1201b2104f0edf82a
Stas SUȘCOV <stas@net.utcluj.ro>
2013-10-14 16:10:08 +0300

    Added support for parallel tests.

---
+++ spec/support/parallel_tests.rb
@@ -0,0 +1,14 @@
+if ENV['WERCKER']
+  require 'parallel_tests'
+
+  if test_env_id = ENV['TEST_ENV_NUMBER']
+    Capybara.server_port =
+      Capybara::Server.new(nil).send(:find_available_port) + test_env_id.to_i
+
+    RSpec.configure do |config|
+      config.before(:each) do
+        SimpleCov.command_name 'RSpec:%s_%s' % [ Process.pid, test_env_id ]
+      end
+    end if defined?(SimpleCov)
+  end
+end
