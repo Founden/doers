100e741affc744f2d477ad5beed44c7f43c1efe6
Stefan Gugurel <stefangugurel@gmail.com>
2013-10-15 17:28:46 +0300

    Implemented last update mixin.

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,7 +1,7 @@
 # [Activity] model serializer
 class ActivitySerializer < ActiveModel::Serializer
-  attributes :id, :slug, :updated_at, :last_update
-  attributes :user_name, :project_title, :board_title, :topic_title, :card_title
+  attributes :id, :slug, :updated_at, :user_name, :project_title
+  attributes :board_title, :topic_title, :card_title
 
   has_one :user, :embed => :id
   has_one :project, :embed => :id
@@ -9,9 +9,4 @@ class ActivitySerializer < ActiveModel::Serializer
   has_one :card, :embed => :id
   has_one :topic, :embed => :id
   has_one :comment, :embed => :id
-
-  # Creates a nice timestamp to indicate when it was last time updated
-  def last_update
-    object.updated_at.to_s(:pretty)
-  end
 end
