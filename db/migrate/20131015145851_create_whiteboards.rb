7f7abc01b97997d5873432bd0e7b1622ff3b08ba
Stas SUȘCOV <stas@net.utcluj.ro>
2013-10-15 20:35:18 +0300

    Created whiteboards.

---
+++ db/migrate/20131015145851_create_whiteboards.rb
@@ -0,0 +1,30 @@
+class CreateWhiteboards < ActiveRecord::Migration
+  def change
+    create_table :whiteboards do |t|
+      t.string :title
+      t.text :description
+      t.references :user, index: true
+      t.references :team, index: true
+
+      t.timestamps
+    end
+
+    remove_column :boards, :author_id
+    remove_column :boards, :parent_board_id
+
+    add_column :comments, :whiteboard_id, :integer
+    add_index :comments, :whiteboard_id
+
+    add_column :activities, :whiteboard_id, :integer
+    add_index :activities, :whiteboard_id
+
+    add_column :boards, :whiteboard_id, :integer
+    add_index :boards, :whiteboard_id
+
+    add_column :topics, :whiteboard_id, :integer
+    add_index :topics, :whiteboard_id
+
+    add_column :cards, :alignment, :bool
+    add_index :cards, :alignment
+  end
+end
