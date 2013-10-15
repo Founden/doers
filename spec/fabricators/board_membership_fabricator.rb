503350f56e19a749da2dd76e44b2ecfd7e55cbb8
Stas SUȘCOV <stas@net.utcluj.ro>
2013-10-15 23:30:25 +0300

    Updated board membership tests.

--- spec/fabricators/board_membership_fabricator.rb
+++ spec/fabricators/board_membership_fabricator.rb
@@ -1,5 +1,5 @@
 Fabricator(:board_membership, :class_name => BoardMembership) do
   creator(:fabricator => :user)
   user
-  board {|attrs| Fabricate(:board, :author => attrs[:creator])}
+  board {|attrs| Fabricate(:board, :user => attrs[:creator])}
 end
