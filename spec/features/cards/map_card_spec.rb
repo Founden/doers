993c7466882313e5d3f02e3ae076150576ff139d
Stas SUȘCOV <stas@net.utcluj.ro>
2013-10-17 14:46:47 +0300

    Updated card tests to reflect textarea changes.

--- spec/features/cards/map_card_spec.rb
+++ spec/features/cards/map_card_spec.rb
@@ -15,7 +15,8 @@ feature 'Map', :js, :slow do
 
     scenario 'is shown with details' do
       expect(page).to have_css('.card', :count => 1)
-      expect(page.source).to include(card.title)
+      expect(page.find('.card-field-title').value).to eq(card.title)
+      expect(page.find('.card-field-description').value).to eq(card.content)
       expect(page.source).to match(/maps\.googleapis\.com/)
       expect(page.source).to include("#{card.longitude}")
       expect(page.source).to include("#{card.latitude}")
