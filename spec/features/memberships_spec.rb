04b9a362a48f781f9ac600ba701c520eb5484f8b
Stefan Gugurel <stefangugurel@gmail.com>
2013-10-31 19:43:04 +0200

    Updated membership feature tests.

--- spec/features/memberships_spec.rb
+++ spec/features/memberships_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature 'Memberships', :js, :slow do
+feature 'Memberships', :js, :focus do
   background do
     sign_in_with_angel_list
   end
@@ -15,16 +15,16 @@ feature 'Memberships', :js, :slow do
     context 'member is added' do
       scenario 'adds an user to memberships' do
         member = Fabricate(:user)
-        expect(page).to have_css('.member-list .member-item', :count => 1)
+        expect(page).to have_css('.project-member-item', :count => 1)
 
-        page.find('.member-item-add').click
-        within('.member-add-form') do
+        page.find('.project-invitee-add').click
+        within('.project-invitee-form') do
           fill_in :email, :with => member.email
         end
-        page.find('.add-member').click
+        page.find('.invite-member').click
 
         sleep(1)
-        expect(page).to have_css('.member-list .member-item', :count => 2)
+        expect(page).to have_css('.project-member-item', :count => 2)
         expect(member.accepted_memberships.reload.count).to_not eq(0)
       end
     end
@@ -32,18 +32,19 @@ feature 'Memberships', :js, :slow do
     context 'email is invited' do
       scenario 'sends an invitation' do
         email = Faker::Internet.email
-        expect(page).to have_css('.member-list .member-item', :count => 1)
+        expect(page).to have_css('.project-invitee', :count => 0)
 
-        page.find('.member-item-add').click
-        within('.member-add-form') do
+        page.find('.project-invitee-add').click
+        within('.project-invitee-form') do
           fill_in :email, :with => email
         end
-        page.find('.add-member').click
+        page.find('.invite-member').click
 
-        expect(page).to have_css('.member-list .member-item', :count => 1)
+        expect(page).to have_css('.project-invitee', :count => 1)
         sleep(1)
         expect(Invitation.find_by(:email => email)).to_not be_nil
       end
     end
+
   end
 end
